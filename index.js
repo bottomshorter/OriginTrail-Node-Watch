8c765eb45cf75c97bc50bb046fa9ab6138995746
bottomshorter <lamvanthony@gmail.com>
2020-01-31 09:13:49 -0800

    alpha version patch

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var NodesWatched = []
 //Basic Commands
 //Bot Start Message
 bot.start((ctx) => {
-    startBackgroundCheck(ctx)
-    ctx.reply('✌🏼Welcome, fellow Tracer✌🏼 \n I am merely a bot, so please dont abuse me. \n \nMy sole purpose in this life is to help you watch your nodes! I will allow you to subscribe for notifications regarding downtimes, node updates, low Ethereum balance, or payouts when your node(s) completes a job! \n \nWith that being said, you probably want to add your node(s) using the /add command. \n \nUse the following syntax: \n\n/add ERC725 \n \nReplace ERC725 with the ERC725 address assigned to your node. \n \nExample: /add 123.456.789.012 \n \nAfter that, you can send me some more commands to configure the notifications or check your node metrics.  \n \nExamples: \n- /nodedetails: To get a detailed view of your nodes \n- /tokenstatus: To check your staked tokens vs your locked tokens \n- /history: To show information about your payout history \n\nTo see the menu of the possible commands just send me /help \n \nMy creator, @bottomshorter, put a lot of time into this. Send him a tip as appreciation! ✌🏼 ☕️ & 🍔 ERC20 => 0x0d4C19fb34d4B8A45004E7A2a23B9d7283b32304')
+    // startBackgroundCheck(ctx)
+    ctx.reply('✌🏼Welcome, fellow Tracer✌🏼 \n I am merely a bot, so please dont abuse me. \n \nMy sole purpose in this life is to help you watch your nodes! This version is currently in alpha and functionality will be LIMITED for a indefinite period of time, meaning updates will only come as I have free time to contribute. I encourage those with development experience to contribute to the GitHub and help me develop features. Feel free to contact me or make a pull request to the Github repo. \n \nWith that being said, you probably want to add your node(s) using the /add command. \n \nUse the following syntax: \n\n/add ERC725 \n \nReplace ERC725 with the ERC725 address assigned to your node. \n \nExample: /add 0xb7695a0a431282a124fbd301b67625cc178ba57c  \n \nCommands: \n- /details: To get a detailed view of your nodes \n- /recentjobs: To check the recent jobs won within the last 7 days across ALL of your nodes \n\nTo see the menu of the possible commands just send me /help')
 })
 
 //Bot Help Message
-bot.help((ctx) => ctx.reply('These are the following commands: \n\n- /add: Add ERC725 identity/desired nickname for node to watchlist \n- /detail: To get a detailed view of your nodes \n- /tokenstatus: To check your staked tokens vs your locked tokens \n- /history: To show information about your payout history'))
+bot.help((ctx) => ctx.reply('These are the following commands: \n\n- /add: Add ERC725 identity to watchlist \n- /details: To get a detailed view of your nodes \n- /recentjobs: To check the recent jobs won within the last 7 days across ALL of your nodes \n-Other commands are currently under development...'))
 
 
 //Listen To These Commands
@@ -33,7 +33,7 @@ bot.command('/add', (ctx) => {
     //console.log(userERC725);
 
 
-    axios.get('https://api.othub.info/api/nodes/dataholders/' + userERC725)
+    axios.get('https://othub-api.origin-trail.network/api/nodes/DataHolders/' + userERC725)
         .then(response => {
             //console.log(response.data);
             console.log(response.data.Identity);
@@ -50,17 +50,17 @@ bot.command('/add', (ctx) => {
 })
 
 //View Nodes command
-bot.command('/nodedetails', (ctx) => {
+bot.command('/details', (ctx) => {
     //Reply the Amount of nodes 
     ctx.reply('Number of nodes saved: ' + NodesWatched.length);
     var i;
     var NodeNum = 1;
     for (i = 0; i < NodesWatched.length; i++){
-    axios.get('https://api.othub.info/api/nodes/dataholders/' + NodesWatched[i])
+    axios.get('https://othub-api.origin-trail.network/api/nodes/DataHolders/' + NodesWatched[i])
         .then(response => {
             
             // console.log(response.data);
-            ctx.reply("Node - " + NodeNum++ + "\n\nIdentity: " + response.data.Identity + "\n\nTotal Won Offers: " + response.data.TotalWonOffers + "\nWon Offers(Last 7 days): " + response.data.WonOffersLast7Days + "\n\nStaked Tokens: " + response.data.StakeTokens + "\nTokens locked in jobs: " + response.data.StakeReservedTokens + "\n\nManagement Wallet: " + response.data.ManagementWallet);
+            ctx.reply("Node - " + NodeNum++ + "\n\nIdentity: " + response.data.Identity + "\n\nTotal Won Offers: " + response.data.TotalWonOffers + "\nWon Offers(Last 7 days): " + response.data.WonOffersLast7Days + "\n\nStaked Tokens: " + (Math.round(response.data.StakeTokens * 100) / 100).toFixed(2) + "\nTokens Paid: " + (Math.round(response.data.PaidTokens * 100) / 100).toFixed(2) + "\nTokens locked in jobs: " + (Math.round(response.data.StakeReservedTokens * 100) / 100).toFixed(2) + "\n\nManagement Wallet: " + response.data.ManagementWallet);
         })
         .catch(error => {
             console.log(error);
@@ -71,12 +71,35 @@ bot.command('/nodedetails', (ctx) => {
     }
 });
 
+//Recent Jobs command
+bot.command('/recentjobs', (ctx) => {
+    //Reply to the Amount of nodes added 
+    ctx.reply('Jobs won across all nodes within the last 7 days:');
+    var i;
+    var JobNum = 0;
+    
+    for (i = 0; i < NodesWatched.length; i++){
+    axios.get('https://othub-api.origin-trail.network/api/RecentActivity?identity=' + NodesWatched[i])
+        .then(response => {
+            
+            for (i = 0; i < response.data.length; i++){
+            // console.log(response.data);
+            // console.log(Object.keys(response.data));
+            ctx.reply("🎉Job -" + JobNum++  + "🎉" + "\n\nOfferID: " + response.data[i].OfferId + "\n\nToken Amount Per Holder: " + (Math.floor((response.data[i].TokenAmountPerHolder)*100)/100));
+        }})
+        .catch(error => {
+            console.log(error);
+        });
+        //console.log(NodesWatched);
+    }
+});
+
 //Launch bot
 bot.launch()
 
 console.log("bot running...")
 
-NodesWatched.push("0x20d4a7bebb36905212cd5324e501b43666186e22") //DEBUG
+NodesWatched.push() //DEBUG
 function onNewEntry(ctx) {
     return function(newOffers) {
         if (isNotEmpty(newOffers))
