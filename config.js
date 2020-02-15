97cb5c8c819a6d4ea6d45fffbe6fb4a9cff961c1
Cressi Dave <>
2020-02-15 20:53:32 +0000

    Fix typo

--- config.js
+++ config.js
@@ -5,12 +5,10 @@
     
 	// Nodes
 	// Add node ID Strings to Array
-	config.nodes = [	
-					 
-				   ];
+	config.nodes = ['0x51b604257d7bfa4fbaf7b4d127e1730e31e60b15'];
     
 	// Telegram Key
-	config.telgram = 'TELEGRAM KEY';
+	config.telegram = '667171603:AAHvoGw3MfSNQ2AseXfrOHBzg1pB4rzW2i0';
 
 
 	// Export Module
